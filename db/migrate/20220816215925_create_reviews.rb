48e88654434fcf6c30142f77860a25afbc6fd824
arturo <ahpareja@gmail.com>
2022-08-16 17:32:50 -0500

    Add columns to book and review models

--- db/migrate/20220816215925_create_reviews.rb
+++ db/migrate/20220816215925_create_reviews.rb
@@ -1,6 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
+      t.string :author
+      t.string :comment
+      t.reference :book, foreign_key: true
 
       t.timestamps
     end
