20e13a3a791d45a5db443666ebf600ff652c1828
arturo <ahpareja@gmail.com>
2022-08-16 18:55:13 -0500

    Edit routes and styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :books 
+  root 'books#index'
+  resources :books, only: [:index, :show] 
 end
